fe2a068a064de58a0d9c922b277a0a0c82fe5c16
Juan Mendieta <juanmsn@live.com>
2017-09-22 13:37:25 -0500

    Creacion de la clase Mensaje

---
+++ src/Mensaje.java
@@ -0,0 +1,16 @@
+/*
+ * Clase para administrar los mensajes que retorna el sistema
+ * Autor: Juan Mendieta
+ * Fecha de modificacion: 21/09/2017
+ * */
+
+public final class Mensaje {
+	public static final String SAVE_OK = "Sus Datos se han guardado satisfactoriamente.";
+	public static final String REMOVE_OK = "Se ha eliminado satisfactoriamente.";
+	public static final String EMPTY_FIELD = "Existen campos vacios.";
+	public static final String INVALID_FIRSTNAME = "El Nombre ingresado no es válido.";
+	public static final String INVALID_LASTNAME = "El Apellido ingresado no es válido.";
+	public static final String INVALID_CEDULA = "La Cédula ingresada no es válido.";
+	public static final String INVALID_SEX = "El Sexo ingresado no es válido.";
+	public static final String INVALID_ID = "El Id no es válido.";
+}
